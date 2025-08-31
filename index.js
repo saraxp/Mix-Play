c4651db7800a1ef038ca134cad13ea7f39ff993e
saraxp <sara.ap408@gmail.com>
2025-08-31 15:46:40 +0530

    Icon changed

--- index.js
+++ index.js
@@ -395,7 +395,7 @@ async function showAlbumTracks(albumName, artistName) {
         fullList.innerHTML = `
             <div class="album-header">
                 <button id="backToAlbumsBtn" class="back-btn">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(12, 20, 19)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
                 Back to Albums</button>
                 <h3>${albumName}</h3>
                 <p>${artistName}</p>
@@ -445,7 +445,7 @@ async function showAlbumTracks(albumName, artistName) {
         fullList.innerHTML = `
             <div class="album-header">
                 <button id="backToAlbumsBtn" class="back-btn">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(12, 20, 19)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
                 Back to Albums</button>
                 <h3>${albumName}</h3>
                 <p>${artistName}</p>
